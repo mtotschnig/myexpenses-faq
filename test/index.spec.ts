d7fc734cc9bf9e390d43d00ab3dc084761d767b6
Michael Totschnig <michael@totschnig.org>
2024-12-05 23:48:43 +0100

    Adapt test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -7,19 +7,25 @@ import worker from '../src/index';
 // `Request` to pass to `worker.fetch()`.
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
-		const request = new IncomingRequest('http://example.com');
+describe('FAQ worker', () => {
+	it('responds with not found', async () => {
+		const request = new IncomingRequest('http://example.com/hudriwusch');
 		// Create an empty context to pass to `worker.fetch()`.
 		const ctx = createExecutionContext();
 		const response = await worker.fetch(request, env, ctx);
 		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		expect(await response.status).toBe(404);
 	});
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch('https://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
+        it('responds with not found', async () => {
+                const request = new IncomingRequest('http://example.com/sync-debug');
+                // Create an empty context to pass to `worker.fetch()`.
+                const ctx = createExecutionContext();
+                const response = await worker.fetch(request, env, ctx);
+                // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+                await waitOnExecutionContext(ctx);
+                expect(await response.status).toBe(301);
+        });
+
 });
