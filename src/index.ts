9e3cdc0168348a25e46122062b06c64be4c1094e
Michael Totschnig <michael@totschnig.org>
2024-12-13 22:03:55 +0100

    additional short URLs

--- src/index.ts
+++ src/index.ts
@@ -12,20 +12,27 @@
  */
 
 const shortURLs = {
-  "data-transfer": "FAQ%3A-Data#how-to-transfer-data-to-a-new-device",
-  "sync-debug": "FAQ%3A-Synchronization#synchronization-stopped-working-how-can-i-find-out-why",
+  "data-transfer": "Data#how-to-transfer-data-to-a-new-device",
+  "sync-debug": "Synchronization#synchronization-stopped-working-how-can-i-find-out-why",
+  "sync-budget": "Synchronization#synchronization-of-budgets",
+  "data-attachments": "Data#pros-and-cons-of-copying-or-linking-attachments",
+  "data-share": "Data#what-are-the-different-share-options",
+  "pdf-layout": "Import-Export#how-to-configure-header-and-footer-on-the-pdf-printout",
+  "data-encryption": "Data#how-does-database-encryption-work",
+  "sync-xiaomi": "Synchronization#synchronization-does-not-work-on-my-xiaomi-or-tcl-or-similar-device-why",
+  "parties-merge": "Data#strategies-for-merging-duplicate-parties",
+  "category-types": "Data#expense-and-income-categories"
+
 } as Record<string, string>
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		 const url = new URL(request.url)
 
-    const redirectURL = shortURLs[url.pathname.substring(1)]
+    const url = new URL(request.url)
+    const path = url.pathname.substring(1)
 
-    if (!redirectURL) {
-      return new Response("Not found", { status: 404 });
-    }
+    const redirectURL = path.length == 0 ? "FAQ" : ("FAQ:-" + (shortURLs[url.pathname.substring(1)] || path))
 
     return Response.redirect("https://github.com/mtotschnig/MyExpenses/wiki/" + redirectURL, 301)
-	},
+  },
 } satisfies ExportedHandler<Env>;
